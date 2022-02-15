9833093918c46c722dac958eaa50913f68ba4f32
Mika Rinne <mika.rinne@oracle.com>
2022-02-15 11:06:15 +0200

    changes

---
+++ src/main/java/kuberedisclient/KuberedisclientController.java
@@ -0,0 +1,29 @@
+package kuberedisclient;
+
+import io.lettuce.core.RedisClient;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.DefaultHttpClientConfiguration;
+import io.micronaut.http.client.HttpClientConfiguration;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.ApplicationConfiguration;
+import jakarta.inject.Inject;
+
+@Controller("/redis")
+public class KuberedisclientController {
+
+    @Inject
+    RedisClient redisClient;
+
+    @Get("/test")
+    String test() {
+        redisClient.connect();
+        return "ok";
+    }
+}
+
